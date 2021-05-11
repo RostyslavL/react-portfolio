bbd9cbaab1eeb2811c027cc709f8f0a1089e5d38
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-11 16:16:29 +0200

    feat: create sidebar  menu with styles & animations transition using scss and 'useState' hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
 import Works from './components/works/Works'
 import Contact from './components/contact/Contact'
+import Menu from './components/menu/Menu'
 import "./app.scss"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <div className="sections">
           <Intro />
           <Portfolio />
