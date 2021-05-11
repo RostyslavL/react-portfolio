347cc127ac46faf5ad69d2493e5b940fcc9626fb
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-11 13:34:00 +0200

    feat: create main components markup, create scss styles for each component, add navbar with animation & menu hamburger

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react'
+import Topbar from './components/topbar/TopBar'
+import Intro from './components/intro/Intro'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Works from './components/works/Works'
+import Contact from './components/contact/Contact'
+import "./app.scss"
+
+function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <div className="sections">
+          <Intro />
+          <Portfolio />
+          <Testimonials />
+          <Works />
+          <Contact />
+        </div>
+    </div>
+  )
+}
+
+export default App
