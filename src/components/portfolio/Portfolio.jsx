0d5bdc7935342bb15804a5c659220636b544fdf6
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-13 15:28:38 +0200

    feat: create Portfolio section, added animations, styles , images & links to additional external projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,93 @@
 import "./portfolio.scss"
+import  PortfolioList from '../portfolioList/PortfolioList'
+import {useState, useEffect} from 'react'
+import {
+    fullstackPortfolio, 
+    webPortfolio,
+    mobilePortfolio,
+    backEndPortfolio,
+    frontendPortfolio
+} from '../../data'
+
 
 export default function Portfolio() {
+    
+    const [selected, setSelected] = useState("featured")
+    const [data, setData] = useState([])
+
+    const list = [
+        {        
+            id:'fullstack',
+            title: 'Fullstack Apps',
+        },
+        {        
+            id:'web',
+            title: 'Web Apps',
+        },
+        {        
+            id:'frontend',
+            title: 'Frontend Apps & Landing Pages',
+        },
+        {        
+            id:'backend',
+            title: 'Backend Apps',
+        },
+        {        
+            id:'mobile',
+            title: 'Mobile Apps',
+        },
+    ]
+
+    useEffect(() => {
+        switch(selected){
+            case 'fullstack':
+            setData(fullstackPortfolio)
+            break;
+            case 'web':
+            setData(webPortfolio)
+            break;
+            case 'frontend':
+            setData(frontendPortfolio)
+            break;
+            case 'mobile':
+            setData(mobilePortfolio)
+            break;
+            case 'backend':
+            setData(backEndPortfolio)
+            break;
+            default:
+            setData(webPortfolio)
+        }
+        
+    }, [selected])
     return (
         <div className="portfolio" id="portfolio">
-            asd
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item) => ( 
+                    <PortfolioList 
+                        title={item.title} 
+                        active={selected === item.id} 
+                        setSelected={setSelected}
+                        id={item.id}
+                    />
+                ))}
+            </ul>
+            <div className="container">
+                {data.map((d) =>(
+                    <div className="item">
+                        <img 
+                        src={d.img} 
+                        alt={d.title} 
+                        onClick={()=> window.open(`${d.link}`, "_blank")}
+                        />
+                    </div>
+                ))} 
+            </div>
         </div>
     )
 }
+
+
+
+
---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const fullstackPortfolio = [
+    {
+        id:1,
+        title:'eComerce fullstack MERN App Project',
+        img:'img/eComerce_MERN_Project.jpg',
+        link: 'https://ecomercemernproshopapp.herokuapp.com/'
+    },
+    {
+        id:2,
+        title:'Next JS fullstack Music Events App Project',
+        img:'img/next_js_dj_music_events.jpg',
+        link: 'https://ecomercemernproshopapp.herokuapp.com/'
+    },
+]
+export const webPortfolio = [
+    {
+        id:1,
+        title:'eComerce fullstack MERN App Project',
+        img:'img/eComerce_MERN_Project.jpg',
+        link: 'https://ecomercemernproshopapp.herokuapp.com/'
+    },
+    {
+        id:2,
+        title:'Next JS fullstack Music Events App Project',
+        img:'img/next_js_dj_music_events.jpg',
+        link: 'https://ecomercemernproshopapp.herokuapp.com/'
+    },
+    {
+        id:3,
+        title:'Next JS Strapi Backend integration for Music Events App Project',
+        img:'img/strapi_backed_dj_music_eventsjpg.jpg',
+        link: 'https://dj-music-events-backend.herokuapp.com/admin/plugins/content-manager/collectionType/application::events.events?page=1&pageSize=10&_sort=name:ASC'
+    },
+    {
+        id:4, 
+        title:'JS Isenct Catch Game',
+        img:'img/isenct_catch_game.jpg',
+        link: 'https://rostyslavl.github.io/Isenct-catch-game/'
+    },
+    {
+        id:5, 
+        title:'React Quiz Creation App',
+        img:'img/react_quiz.jpg',
+        link: 'https://react-quiz-refactored.firebaseapp.com/quiz-creator'
+    },
+]
+
+export const mobilePortfolio = [
+    {
+        
+    },
+    {
+
+    },
+]
+
+export const backEndPortfolio = [
+    {
+        id:1,
+        title:'Next JS Strapi Backend integration for Music Events App Project',
+        img:'img/strapi_backed_dj_music_eventsjpg.jpg',
+        link: 'https://dj-music-events-backend.herokuapp.com/admin/plugins/content-manager/collectionType/application::events.events?page=1&pageSize=10&_sort=name:ASC'
+    },
+]
+
+export const frontendPortfolio = [
+    {
+        id:1,
+        title:'Landing Page Project ',
+        img:'img/landing_page_project_practise.jpg',
+        link: 'https://rostyslavl.github.io/Landing-page-project-practise/'
+    },
+    {
+        id:2,
+        title:'CV Page From Rolling Scopes School FE2021 EUROPE',
+        img:'img/rsschool_cv_page.jpg',
+        link: 'https://rostyslavl.github.io/rsschool-cv/'
+    },    
+    {
+        id:3,
+        title:'Previous Portfolio Page',
+        img:'img/first_portfolio_page.jpg',
+        link: 'https://firstportfilio.web.app/'
+    },
+    {
+        id:4,
+        title:'NetFlix Mobile Navigation Landing Page',
+        img:'img/netflix_landing_page_clone.jpg',
+        link: 'https://netflixlandingpageclone.web.app/'
+    },
+    
+]
